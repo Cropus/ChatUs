3f537bed8542c6c4f7a3296e1b8d0804fe793cd5
Cropus <mr.flashmob@bk.ru>
2021-04-25 18:36:43 +0300

    Add Chats from GraphQL

--- components/ChatListItem/index.tsx
+++ components/ChatListItem/index.tsx
@@ -1,35 +1,68 @@
-import React from 'react';
-import { View, Text, Image, TouchableWithoutFeedback } from 'react-native';
-import { ChatRoom } from '../../types';
+import React, {useEffect, useState} from 'react';
+import {
+    View,
+    Text,
+    Image,
+    TouchableWithoutFeedback
+} from "react-native";
+import { ChatRoom } from "../../types";
 import styles from "./style";
 import moment from "moment";
-import {useNavigation} from '@react-navigation/native';
+import { useNavigation } from '@react-navigation/native';
+import {
+    Auth,
+} from 'aws-amplify';
 
 export type ChatListItemProps = {
     chatRoom: ChatRoom;
 }
 
 const ChatListItem = (props: ChatListItemProps) => {
-    const {chatRoom} = props;
-    const user = chatRoom.users[1];
+    const { chatRoom } = props;
+    const [ otherUser, setOtherUser] = useState(null);
+
     const navigation = useNavigation();
+
+    useEffect(() => {
+        const getOtherUser = async () => {
+            const userInfo = await Auth.currentAuthenticatedUser();
+            if (chatRoom.chatRoomUsers.items[0].user.id === userInfo.attributes.sub) {
+                setOtherUser(chatRoom.chatRoomUsers.items[1].user);
+            } else {
+                setOtherUser(chatRoom.chatRoomUsers.items[0].user);
+            }
+        }
+        getOtherUser();
+    }, [])
+
     const onClick = () => {
         navigation.navigate('ChatRoom', {
             id: chatRoom.id,
-            name: user.name,
-            image: user.imageUri,
-        });
+            name: otherUser.name,
+        })
+    }
+
+    if (!otherUser) {
+        return null;
     }
 
     return (
         <TouchableWithoutFeedback onPress={onClick}>
             <View style={styles.container}>
                 <View style={styles.leftContainer}>
-                    <Image source={require('../../assets/images/icon.png')} style={styles.avatar} />
+                    <Image source={{ uri: otherUser.imageUri }} style={styles.avatar}/>
+
                     <View style={styles.midContainer}>
-                        <Text style={styles.username}>{user.name}</Text>
-                        <Text numberOfLines={1} style={styles.lastMessage}>{chatRoom.lastMessage.content}</Text>
+                        <Text style={styles.username}>{otherUser.name}</Text>
+                        <Text
+                            numberOfLines={2}
+                            style={styles.lastMessage}>
+                            {chatRoom.lastMessage
+                                ? `${chatRoom.lastMessage.user.name}: ${chatRoom.lastMessage.content}`
+                                : ""}
+                        </Text>
                     </View>
+
                 </View>
 
                 <Text style={styles.time}>
