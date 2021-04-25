67f05da75c4654592f4a4a46de46c9f122496e94
Cropus <mr.flashmob@bk.ru>
2021-04-25 17:47:07 +0300

    Create ChatRoom from Contacts

--- components/ContactListItem/index.tsx
+++ components/ContactListItem/index.tsx
@@ -3,6 +3,8 @@ import { View, Text, Image, TouchableWithoutFeedback } from 'react-native';
 import { User } from '../../types';
 import styles from "./style";
 import {useNavigation} from '@react-navigation/native';
+import {API, Auth, graphqlOperation} from "aws-amplify";
+import {createChatRoom, createChatRoomUser} from "../../src/graphql/mutations";
 
 export type ContactListItemProps = {
     user: User;
@@ -11,15 +13,64 @@ export type ContactListItemProps = {
 const ContactListItem = (props: ContactListItemProps) => {
     const {user} = props;
     const navigation = useNavigation();
-    const onClick = () => {
-        // navigate
+    const onClick = async () => {
+        try {
+
+            const newChatRoomData = await API.graphql(
+                graphqlOperation(
+                    createChatRoom, {
+                        input: {
+                            lastMessageID: ""
+                        }
+                    }
+                )
+            )
+            if (!newChatRoomData.data) {
+                console.log(" Failed to create a chat room");
+                return;
+            }
+            const newChatRoom = newChatRoomData.data.createChatRoom;
+
+
+            await API.graphql(
+                graphqlOperation(
+                    createChatRoomUser, {
+                        input: {
+                            userID: user.id,
+                            chatRoomID: newChatRoom.id,
+                        }
+                    }
+                )
+            )
+
+
+            const userInfo = await Auth.currentAuthenticatedUser();
+            await API.graphql(
+                graphqlOperation(
+                    createChatRoomUser, {
+                        input: {
+                            userID: userInfo.attributes.sub,
+                            chatRoomID: newChatRoom.id,
+                        }
+                    }
+                )
+            )
+
+            navigation.navigate('ChatRoom', {
+                id: newChatRoom.id,
+                name: "Chat",
+            })
+
+        } catch (e) {
+            console.log(e);
+        }
     }
 
     return (
         <TouchableWithoutFeedback onPress={onClick}>
             <View style={styles.container}>
                 <View style={styles.leftContainer}>
-                    <Image source={require('../../assets/images/icon.png')} style={styles.avatar} />
+                    <Image source={{uri: user.imageUri}} style={styles.avatar} />
                     <View style={styles.midContainer}>
                         <Text style={styles.username}>{user.name}</Text>
                         <Text numberOfLines={1} style={styles.status}>{user.status}</Text>
